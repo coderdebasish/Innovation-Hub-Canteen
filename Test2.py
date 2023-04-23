0b039fd62ddd35743b9d65d0242cf7da1b15bf60
Debasish Mohanty <debasish.mohanty169@gmail.com>
2023-04-23 17:24:27 +0530

    dghghf

--- Test2.py
+++ Test2.py
@@ -2,17 +2,12 @@ from docx import Document
 from docx.shared import Inches
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 
-# create a new document
+
 document = Document("abcd.docx")
 
-# add a heading
-# document.add_heading('My Image')
-
-# add an image
 paragraph = document.add_paragraph()
 paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
 run = paragraph.add_run()
 run.add_picture('upi.png', width=Inches(0.3), height=Inches(0.3), )
 
-# save the document
 document.save('my_doc.docx')
